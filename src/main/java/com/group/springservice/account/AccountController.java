7f69adf366f540cfef3c75d9865b6dbd07091408
yuda3 <yuda3@hotmail.com>
2024-06-27 21:47:51 +0900

    SecurityConfig, View Test

---
+++ src/main/java/com/group/springservice/account/AccountController.java
@@ -0,0 +1,14 @@
+package com.group.springservice.account;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccountController {
+
+    @GetMapping("/sign-up")
+    public String signup(Model model) {
+        return "account/sign-up";
+    }
+}
---
+++ src/test/java/com/group/springservice/account/AccountControllerTest.java
@@ -0,0 +1,30 @@
+package com.group.springservice.account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @Test
+    void signUpForm() throws Exception {
+        mockMvc.perform(get("/sign-up"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("account/sign-up"));
+    }
+
+}
