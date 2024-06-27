fdbc7d2ea1faae4ada7f0643cd15a155e5dd38a7
yuda3 <yuda3@hotmail.com>
2024-06-27 21:33:48 +0900

    Account

---
+++ src/main/java/com/group/springservice/domain/Account.java
@@ -0,0 +1,54 @@
+package com.group.springservice.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter@Setter
+@EqualsAndHashCode(of = "id")
+@Builder@AllArgsConstructor@NoArgsConstructor
+public class Account {
+
+    @Id
+    @GeneratedValue()
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+    @Column(unique = true)
+    private String nickname;
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob@Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpratedByEmail;
+
+    private boolean studyUpratedByWeb;
+
+
+
+
+}
